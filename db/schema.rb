fc71ddd60a1a3ad11088aac2d2c86c3458607ccf
Jason Hobbs <grimmstede@feemon.com>
2015-11-04 22:13:48 -0600

    added admin to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151104043935) do
+ActiveRecord::Schema.define(version: 20151105041308) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20151104043935) do
     t.string   "unconfirmed_email"
     t.integer  "failed_attempts",        default: 0,  null: false
     t.datetime "locked_at"
+    t.boolean  "admin"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
