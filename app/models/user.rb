c0e7caf79a1d915ac6a19bc44863db988ed98cd4
Jason Hobbs <grimmstede@feemon.com>
2015-11-05 20:13:21 -0600

    sign in with username or email

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,22 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable, :lockable, :timeoutable
 
+  validates_uniqueness_of :username
+  validates_presence_of :username
+  validates :username, length: { in: 4..20 }
+  attr_accessor :login
+
   def gravatar_id
   	Digest::MD5::hexdigest(email.downcase)
   end
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions).first
+    end
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151105041308) do
+ActiveRecord::Schema.define(version: 20151106013932) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,10 +36,12 @@ ActiveRecord::Schema.define(version: 20151105041308) do
     t.integer  "failed_attempts",        default: 0,  null: false
     t.datetime "locked_at"
     t.boolean  "admin"
+    t.string   "username"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
 end
