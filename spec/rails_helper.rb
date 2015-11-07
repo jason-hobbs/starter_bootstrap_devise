d82bbc799575d30eeb1c3af227af97e2687f4307
Jason Hobbs <grimmstede@feemon.com>
2015-11-07 00:08:42 -0600

    rspec config changes

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,12 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
+require 'simplecov'
+SimpleCov.start
+
+RSpec.configure do |config|
+  config.include Devise::TestHelpers, type: :controller
+end
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -25,6 +31,13 @@ require 'rspec/rails'
 # Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
 
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
@@ -48,5 +61,7 @@ RSpec.configure do |config|
   #
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
+  config.use_transactional_fixtures = false
   config.infer_spec_type_from_file_location!
+  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 end
