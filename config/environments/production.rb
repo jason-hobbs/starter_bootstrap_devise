6a3671466ef73259d43a7489aad18b2c27d44747
Jason Hobbs <grimmstede@feemon.com>
2015-11-08 16:58:54 -0600

    changed default url in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,5 +89,5 @@ Rails.application.configure do
     password: ENV["GMAIL_PASSWORD"]
   }
 
-  config.action_mailer.default_url_options = { host: "localhost:3000" }
+  config.action_mailer.default_url_options = { host: "starter-bootstrap-devise.herokuapp.com" }
 end
