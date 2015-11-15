f0db5aca2fe2b42e46d4d7a3df02d016694199ce
Jason Hobbs <grimmstede@feemon.com>
2015-11-15 17:22:32 -0600

    finished admin tests

--- spec/controllers/admin_controller_spec.rb
+++ spec/controllers/admin_controller_spec.rb
@@ -8,11 +8,32 @@ RSpec.describe AdminController, type: :controller do
   context "when an admin" do
     before do
       @request.env["devise.mapping"] = Devise.mappings[:admin]
+      @request.env["devise.mapping"] = Devise.mappings[:user]
       sign_in FactoryGirl.create(:admin)
     end
     it "shows the admin index view" do
       get :index
       expect(response).to render_template(:index)
+    end
+    it "edits a user" do
+      user = FactoryGirl.create(:user)
+      get :edit, format: user
+      expect(response).to render_template(:edit)
+    end
+    it "updates a user" do
+      user = FactoryGirl.create(:user)
+      patch :update, format: user, user: {:username => "test23r"}
+      expect(response).to redirect_to(admin_path)
+    end
+    it "doesn't update a user when the email is empty" do
+      user = FactoryGirl.create(:user)
+      patch :update, format: user, user: {:email => ""}
+      expect(response).to render_template(:edit)
+    end
+    it "can delete a user" do
+      user = FactoryGirl.create(:user)
+      delete :destroy, format: user
+      expect(response).to redirect_to(admin_path)
     end
   end
 
