a92b19d3d70da3bcf2e1edbda2f71ba3a8165f37
Jason Hobbs <grimmstede@feemon.com>
2015-11-17 22:20:56 -0600

    added seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(email: "admin@example.com", username: "Admin User", password: "password", confirmed_at: Date.today, admin: true)
+User.create(email: "user@example.com", username: "Regular User", password: "password", confirmed_at: Date.today)
