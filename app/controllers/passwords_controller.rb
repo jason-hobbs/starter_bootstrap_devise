fbb43b5595f0375f581520174ac41a552624bb6f
Jason Hobbs <jason.hobbs@digisanctum.com>
2019-08-28 20:51:49 -0500

    fix password reset

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,7 +1,7 @@
 class PasswordsController < Devise::PasswordsController
   protected
   def after_sending_reset_password_instructions_path_for(resource_name)
-    gflash :success => "Email send for password reset."
+    flash[:success] = 'Email sent for password reset'
     root_path
   end
 end
